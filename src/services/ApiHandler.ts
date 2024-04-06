3e11e8ad75d692268df3680d99f4e3f9e9bec894
dbruner23 <dbruner23@gmail.com>
2024-04-06 22:48:30 +1300

    change baseURL

--- src/services/ApiHandler.ts
+++ src/services/ApiHandler.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://voxdoxbe.onrender.com",
 });
 
 export const authenticate = async (password: string) => {
